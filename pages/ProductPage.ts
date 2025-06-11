51883939e1bfa9e2be577c685c160b0a0c59ecf5
Hend Elsayed <enghind3@gmail.com>
2025-06-11 17:58:27 +0400

    Update

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -4,7 +4,8 @@ export class ProductPage {
   constructor(private page: Page) {}
 
   async addBackpackToCart() {
-    await this.page.click('#add-to-cart-sauce-labs-backpack');
+   // await this.page.click('#add-to-cart-sauce-labs-backpack');
+    await this.page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
   }
 
   async goToCart() {
